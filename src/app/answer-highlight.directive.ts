01085b2404458a4eb8ab16ecdbad21927f0467a1
miha-9111 <miha-9111@yandex.ru>
2022-01-24 05:34:08 +0300

    Detecting Changes

--- src/app/answer-highlight.directive.ts
+++ src/app/answer-highlight.directive.ts
@@ -1,5 +1,6 @@
 import { Directive, ElementRef, OnInit } from '@angular/core';
 import { NgControl } from "@angular/forms";
+import { map, filter } from "rxjs";
 
 @Directive({
   selector: '[appAnswerHighlight]'
@@ -10,6 +11,13 @@ export class AnswerHighlightDirective implements OnInit{
               private controlName: NgControl) {}
 
   ngOnInit() {
-    console.log(this.controlName.control?.parent);
+    this.controlName.control?.parent?.valueChanges
+      .pipe(
+        map(({ a, b, answer }) => Math.abs((a + b - answer) / (a + b))),
+        filter(value => value < 0.2)
+      )
+      .subscribe((value) => {
+        console.log(value);
+      })
   }
 }
